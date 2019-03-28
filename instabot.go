a1e4a1eee74ea3c442d5a36069caffbdcdf22582
akme <iam@itaddict.ru>
2019-03-28 23:06:18 +0300

    Update instabot.go

--- instabot.go
+++ instabot.go
@@ -239,6 +239,13 @@ func main() {
 					} else {
 						addWatching(bot, db, args, int64(update.Message.From.ID))
 					}
+				case "delwatch":
+					if args == "" {
+						msg.Text = fmt.Sprintf("/delwatch username")
+						bot.Send(msg)
+					} else {
+						deleteKeyFromBucket(db, "watching", args)
+					}
 				case "watching":
 					sendWatching(bot, db, int64(update.Message.From.ID))
 				case "startfollowqueue":
